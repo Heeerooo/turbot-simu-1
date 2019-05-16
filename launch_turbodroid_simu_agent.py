fb2f0817a25fc02e406771422108179a80d330d0
Lior Perez <pertoul@gmail.com>
2019-05-16 09:03:03 +0200

    Implement custom random policy (based on EpsGreedyPolicy, each action followed by a random number of steps where no action is taken or Q action is taken)

--- launch_turbodroid_simu_agent.py
+++ launch_turbodroid_simu_agent.py
@@ -9,14 +9,56 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from rl.agents.dqn import DQNAgent
 from rl.memory import SequentialMemory
-from rl.policy import EpsGreedyQPolicy
+from rl.policy import EpsGreedyQPolicy, LinearAnnealedPolicy
 
 ENV_NAME = 'simu-v0'
 CHECKPOINT_WEIGHTS_FILE = 'dqn_simu-weights_checkpoint.h5f'
 FINAL_WEIGHTS_FILE = 'dqn_simu-weights_one.h5f'
 
-
 WINDOW_LENGTH = 4
+
+class CustomRandomPolicy(EpsGreedyQPolicy):
+    """
+    This random policy is based on EpsGreedyPolicy,
+    but each random action is followed by a random number of steps where:
+       - either no action is taken
+       - or Q action is taken
+
+    Eps Greedy policy either:
+    - takes a random action with probability epsilon
+    - takes current best action with prob (1 - epsilon)
+
+    """
+    def __init__(self, eps=.1):
+        super(CustomRandomPolicy, self).__init__(eps=eps)
+        self.nb_without_action = 0  # Keeps the number of remaining steps without action to be taken
+        self.MAX_STEPS_WITHOUT_ACTION = 8
+
+    def select_action(self, q_values):
+        """Return the selected action
+        # Arguments
+            q_values (np.ndarray): List of the estimations of Q for each action
+        # Returns
+            Selection action
+        """
+        assert q_values.ndim == 1
+        nb_actions = q_values.shape[0]
+
+        if np.random.uniform() < self.eps:
+            if self.nb_without_action > 0:
+                action = 0
+            else:
+                action = np.random.randint(0, nb_actions)
+                self.nb_without_action = np.random.randint(0, self.MAX_STEPS_WITHOUT_ACTION)
+        else:
+            action = np.argmax(q_values)
+
+        # Decrement number of steps without action
+        self.nb_without_action -= 1
+        self.nb_without_action = max(self.nb_without_action, 0)
+
+        return action
+
 # for i in range(10):
 env = gym.make(ENV_NAME)
 # Get the environment and extract the number of actions.
@@ -41,7 +83,11 @@ print(model.summary())
 # even the metrics!
 memory = SequentialMemory(limit=250000, window_length=WINDOW_LENGTH)
 # policy = BoltzmannQPolicy()
-policy = EpsGreedyQPolicy(eps=0.1)
+# policy = EpsGreedyQPolicy(eps=0.1)
+# policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=1000000)
+
+policy = CustomRandomPolicy(eps=1.0)
+
 dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=100,
                target_model_update=1e-2, policy=policy)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
@@ -49,7 +95,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 if os.path.isfile(CHECKPOINT_WEIGHTS_FILE):
     dqn.load_weights(CHECKPOINT_WEIGHTS_FILE)
 
-dqn.test(env, nb_episodes=5, visualize=False)
+# dqn.test(env, nb_episodes=5, visualize=False)
 
 dqn.fit(env, nb_steps=40000, visualize=False, verbose=2, nb_max_episode_steps=200)
 
