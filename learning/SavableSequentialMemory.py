73edb0691884a5c54f67afc601518ef94f2c6f16
Lior Perez <pertoul@gmail.com>
2019-05-25 19:09:14 +0200

    Fix Savable memory issue: switch from np.save to pickle

--- learning/SavableSequentialMemory.py
+++ learning/SavableSequentialMemory.py
@@ -1,33 +1,40 @@
 import os
 
-import numpy as np
 from rl.memory import SequentialMemory
-
+import pickle
 
 class SavableSequentialMemory(SequentialMemory):
+
+    FILENAME = "dqn_simu_memory.pickle"
 
     def __init__(self, limit, filename, **kwargs):
         super().__init__(limit, **kwargs)
         self.filename = filename
 
     def load(self):
-        if os.path.isfile('dqn_simu_memory.npz'):
-            loaded = np.load("dqn_simu_memory.npz")
-            memory = loaded["memory"]
-            lenght = len(memory[0])
-            self.actions.length = lenght
-            self.rewards.length = lenght
-            self.terminals.length = lenght
-            self.observations.length = lenght
-            self.actions.data = memory[0].tolist()
-            self.rewards.data = memory[1].tolist()
-            self.terminals.data = memory[2].tolist()
-            self.observations.data = memory[3].tolist()
-            print("Memory loaded")
+        if os.path.isfile(self.FILENAME):
+            with open(self.FILENAME, 'rb') as handle:
+                loaded = pickle.load(handle)
+                lenght = len(loaded['actions'])
+                self.actions.length = lenght
+                self.rewards.length = lenght
+                self.terminals.length = lenght
+                self.observations.length = lenght
+                self.actions.data = loaded['actions']
+                self.rewards.data = loaded['rewards']
+                self.terminals.data = loaded['terminals']
+                self.observations.data = loaded['observations']
+                print("Memory loaded")
 
     def save(self):
-        memory = np.array([self.actions.data[:self.nb_entries], self.rewards.data[:self.nb_entries],
-                  self.terminals.data[:self.nb_entries], self.observations.data[:self.nb_entries]])
-        np.savez_compressed("dqn_simu_memory.npz", memory=memory)
+        memory = {
+            'actions': self.actions.data[:self.nb_entries],
+            'rewards': self.rewards.data[:self.nb_entries],
+            'terminals': self.terminals.data[:self.nb_entries],
+            'observations': self.observations.data[:self.nb_entries]
+        }
+        with open(self.FILENAME, 'wb') as handle:
+            pickle.dump(memory, handle)
+
         print("Memory saved")
 
